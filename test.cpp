128f7b42af4dbb2a493d06905bec443bceb6cde7
KaigeZheng <kambri2003@qq.com>
2024-11-18 18:55:40 +0800

    Add solver.h:Combine the Advanced Works

--- test.cpp
+++ test.cpp
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "compute_det.h"
+#include "solver.h"
+#include "generator.h"
 
 #define MAX_SIZE 1000
 
-#define ErrorOpeningFIle -1
+#define ErrorOpeningFile -2
 #define SuccessReadMatrix 1
 
 int readMatrix(const char *filename, double *matrix, int n, int m) {
     FILE *file = fopen(filename, "r");
     if(file == NULL) {
         printf("Error opening file!\n");
-        return -1;
+        return ErrorOpeningFile;
     }
 
     // Skip comments
@@ -44,21 +46,24 @@ int readMatrix(const char *filename, double *matrix, int n, int m) {
 }
 
 int main(){
-    double matrix[MAX_SIZE * MAX_SIZE];
+    double A[MAX_SIZE * MAX_SIZE];
+    double b[MAX_SIZE];
+    double x[MAX_SIZE];
     int n = 39;
     int m = 39;
 
     const char *filename = "./data/bcspwr01.mtx";
 
-    int status = readMatrix(filename, matrix, n, m);
+    int status = readMatrix(filename, A, n, m);
+    generate_random_vector(b, n, 0.0, 10.0);
     
     int cnt = 0;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (matrix[(i * m) + j] != 0) {  
+            if (A[(i * m) + j] != 0) {  
                 ++cnt;
-                printf("matrix[%d][%d] = %lf\n", i, j, matrix[(i * m) + j]);
+                printf("A[%d][%d] = %lf\n", i, j, A[(i * m) + j]);
                 //if(i != j) cnt--;
             }
         }
@@ -66,7 +71,22 @@ int main(){
 
     printf("Total Num(Nonzeros) = %d\n", cnt);
 
-    double det = compute_det_gaussion(matrix, n);
+    double det = compute_det_gaussion(A, n);
+
     printf("det = %lf\n", det);
+
+    for(int i = 0; i < n; ++i) printf("b[%d] = %lf\n", i, b[i]);
+
+    if(det != 0) {
+        for(int i = 0; i < n; ++i) {
+            x[i] = 0.0;
+        }
+        jacobi_iteration(A, b, x, n);
+        printf("Solution x = [ \n");
+        for (int i = 0; i < n; i++) {
+            printf("%lf \n", x[i]);
+        }
+        printf("]\n");
+    }
     return 0;
 }
